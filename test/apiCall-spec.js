41a85e790d49928bcbd6fddd85a55a77c2b64915
David Vilaça <davidpvilaca@outlook.com>
2016-08-06 09:39:38 -0300

    ES6 e hotfix prefix

--- test/apiCall-spec.js
+++ test/apiCall-spec.js
@@ -1,69 +1,65 @@
 /**
  * Created by david on 04/03/16.
  */
-/**
- * Created by david on 25/11/15.
- */
-(function() {
-    'use strict';
 
-    var chai = require('chai');
-    var should = chai.should();
-    var expect = chai.expect;
-    var api = require('../lib/apiCall')("", "http://httpbin.org", null);
+'use strict';
 
-    describe('ApiCall', function () {
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const api = require('../lib/apiCall')("", "http://httpbin.org", null);
 
-        describe('CRUD', function () {
+describe('ApiCall', function () {
 
-            it('GET', function (done) {
+    describe('CRUD', function () {
 
-                api.apiCall('/get', api.method.GET, null)
-                    .then(function(data){
-                        expect(data).to.have.ownProperty('data');
-                        expect(data.data).to.be.a('object');
-                        expect(data.status).to.equal(200);
-                        done();
+        it('GET', function (done) {
+
+            api.apiCall('/get', api.method.GET, null)
+                .then(function(data){
+                    expect(data).to.have.ownProperty('data');
+                    expect(data.data).to.be.a('object');
+                    expect(data.status).to.equal(200);
+                    done();
                 });
 
-            });
-            it('POST', function (done) {
-                var body = {post: "apiCall"};
-
-                api.apiCall('/post', api.method.POST, body)
-                    .then(function(data){
-                        expect(data).to.have.ownProperty('data');
-                        expect(data.data).to.be.a('object');
-                        expect(data.status).to.equal(200);
-                        done();
-                });
-
-            });
-            it('PUT', function (done) {
-                var body = {put: "apiCall"};
-
-                api.apiCall('/put', api.method.PUT, body)
-                    .then(function(data){
-                        expect(data).to.have.ownProperty('data');
-                        expect(data.data).to.be.a('object');
-                        expect(data.status).to.equal(200);
-                        done();
-                });
-
-            });
-            it('DELETE', function (done) {
-                var body = {delete: "apiCall"};
-
-                api.apiCall('/delete', api.method.DELETE, body)
-                    .then(function(data){
-                        expect(data).to.have.ownProperty('data');
-                        expect(data.data).to.be.a('object');
-                        expect(data.status).to.equal(200);
-                        done();
-                });
-
-            });
         });
+        it('POST', function (done) {
+            let body = {post: "apiCall"};
 
+            api.apiCall('/post', api.method.POST, body)
+                .then(function(data){
+                    expect(data).to.have.ownProperty('data');
+                    expect(data.data).to.be.a('object');
+                    expect(data.status).to.equal(200);
+                    done();
+                });
+
+        });
+        it('PUT', function (done) {
+            let body = {put: "apiCall"};
+
+            api.apiCall('/put', api.method.PUT, body)
+                .then(function(data){
+                    expect(data).to.have.ownProperty('data');
+                    expect(data.data).to.be.a('object');
+                    expect(data.status).to.equal(200);
+                    done();
+                });
+
+        });
+        it('DELETE', function (done) {
+            let body = {delete: "apiCall"};
+
+            api.apiCall('/delete', api.method.DELETE, body)
+                .then(function(data){
+                    expect(data).to.have.ownProperty('data');
+                    expect(data.data).to.be.a('object');
+                    expect(data.status).to.equal(200);
+                    done();
+                });
+
+        });
     });
-})();
+
+});
